219c8286cb1a8a22b05d701f85f00e1084f7f3e1
Trina Lu <tri613@gmail.com>
2018-06-12 23:23:16 +0800

    add options

--- index.js
+++ index.js
@@ -1,15 +1,32 @@
-const emojione = require('emojione');
-const twemoji = require('twemoji');
+const emojione = require("emojione");
+const twemoji = require("twemoji");
 
-function shortcutToTwemoji(content) {
-  return content.replace(/:(\w*):/g, shortcut => {
+function shortcutToTwemoji(content, options = {}) {
+  const { classname = "", style = {} } = options;
+  const mergedOptions = {
+    classname,
+    style: Object.assign(
+      {},
+      {
+        height: `1em`,
+        width: `1em`,
+        margin: `0 .05em 0 .1em`,
+        "vertical-align": `-0.1e`
+      },
+      style
+    )
+  };
+
+  const styles = Object.keys(mergedOptions.style)
+    .map(key => `${key}: ${mergedOptions.style[key]}`)
+    .join(";");
+
+  return content.replace(/:(.*):/g, shortcut => {
     const toUnicode = emojione.shortnameToUnicode(shortcut);
     const toTwemoji = twemoji.parse(toUnicode);
     const styled = toTwemoji
       .split(/<img class="emoji"/g)
-      .join(
-        `<img class="emoji" style="height: 1em; width: 1em; margin: 0 .05em 0 .1em; vertical-align: -0.1em;"`
-      );
+      .join(`<img class="emoji ${mergedOptions.classname}" style="${styles}"`);
     return styled;
   });
 }
@@ -17,7 +34,7 @@ function shortcutToTwemoji(content) {
 module.exports = {
   mutateSource: ({ markdownNode }, pluginOptions) => {
     const content = markdownNode.internal.content;
-    const result = shortcutToTwemoji(content);
+    const result = shortcutToTwemoji(content, options);
 
     markdownNode.internal.content = result;
     return Promise.resolve(result);
--- index.test.js
+++ index.test.js
@@ -1,10 +1,51 @@
-const { shortcutToTwemoji } = require('./index.js');
+const cheerio = require("cheerio");
+const { shortcutToTwemoji } = require("./index.js");
 
-it('should return emoji from twitter', () => {
-  const content = '\n\nHi! This is my first gatsby blog. Cool! :smile:';
-
+it("should return emoji instead of shortcuts", () => {
+  const content = "Hi! This is my first gatsby blog. Cool! :smile: :+1: :100:";
   const result = shortcutToTwemoji(content);
-  console.log(result);
 
-  expect(result).toEqual(expect.not.stringMatching(/:(\w*):/));
+  const $ = cheerio.load(result);
+  expect($.root().find("img").length).toEqual(2);
+});
+
+it("should not break with invalid shortcuts", () => {
+  const content = "2034-12-23 12:34:53 :sunglasses: :123sdfasdf:";
+  const result = shortcutToTwemoji(content);
+
+  const $ = cheerio.load(result);
+  expect($.root().find("img").length).toEqual(1);
+});
+
+it("should merge option styles", () => {
+  const content = ":123123::heart::heart_eyes:just some text:cool::100:";
+  const style = {
+    "background-color": `#BAD123`
+  };
+  const result = shortcutToTwemoji(content, { style });
+
+  const $ = cheerio.load(result);
+  const $img = $.root().find("img");
+
+  expect($img.css("background-color")).toEqual("#BAD123");
+  expect($img.css("height")).toEqual("1em");
+});
+
+it("should add option's classname", () => {
+  const content = ":123123::heart::heart_eyes:just some text:cool::100:";
+  const style = {
+    "background-color": `#BAD123`
+  };
+  const result = shortcutToTwemoji(content, {
+    classname: "my-class-name another-class-name",
+    style
+  });
+
+  const $ = cheerio.load(result);
+  const $img = $.root().find("img");
+  expect($img.hasClass("another-class-name")).toBeTruthy();
+  expect($img.hasClass("my-class-name")).toBeTruthy();
+  expect($img.hasClass("emoji")).toBeTruthy();
+
+  expect($img.css("background-color")).toEqual("#BAD123");
 });
