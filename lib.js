182f73124ace417d3705da1c92eeaa78cfbde9df
Trina Lu <tri613@gmail.com>
2018-06-15 11:12:26 +0800

    refactor lib code position

--- lib.js
+++ lib.js
@@ -3,35 +3,6 @@ const twemoji = require('twemoji');
 // https://github.com/github/gemoji
 const emojis = require('./emoji.json');
 
-function shortcutToUnicode(shortcut) {
-  const alias = shortcut.replace(/:/g, '');
-  const result = emojis.find(emoji => emoji.aliases.includes(alias));
-  return result ? result.emoji : null;
-}
-
-function replaceShortcut(content, replacement) {
-  let result = content;
-  content.replace(/(?=(:[\w\-+]+:))/g, (_, substr) => {
-    result = result.replace(substr, replacement);
-  });
-  return result;
-}
-
-function parseTwemoji(content, classname, styles) {
-  const twitterEmoji = twemoji.parse(content);
-  const $ = cheerio.load(twitterEmoji, { xmlMode: true });
-
-  const $emojis = $('img.emoji');
-  $emojis.addClass(classname).css(styles);
-  $emojis.each((i, el) => {
-    const $emoji = $(el);
-    const unicode = encodeURIComponent($emoji.attr('alt'));
-    $emoji.attr('alt', unicode);
-  });
-
-  return $.html();
-}
-
 function shortcutToTwemoji(content, { classname = '', style = {} } = {}) {
   const defaultStyle = {
     height: `1em`,
@@ -55,6 +26,35 @@ function shortcutToTwemoji(content, { classname = '', style = {} } = {}) {
     const $emoji = $(el);
     const alt = decodeURIComponent($emoji.attr('alt'));
     $emoji.attr('alt', alt);
+  });
+
+  return $.html();
+}
+
+function replaceShortcut(content, replacement) {
+  let result = content;
+  content.replace(/(?=(:[\w\-+]+:))/g, (_, substr) => {
+    result = result.replace(substr, replacement);
+  });
+  return result;
+}
+
+function shortcutToUnicode(shortcut) {
+  const alias = shortcut.replace(/:/g, '');
+  const result = emojis.find(emoji => emoji.aliases.includes(alias));
+  return result ? result.emoji : null;
+}
+
+function parseTwemoji(content, classname, styles) {
+  const twitterEmoji = twemoji.parse(content);
+  const $ = cheerio.load(twitterEmoji, { xmlMode: true });
+
+  const $emojis = $('img.emoji');
+  $emojis.addClass(classname).css(styles);
+  $emojis.each((i, el) => {
+    const $emoji = $(el);
+    const unicode = encodeURIComponent($emoji.attr('alt'));
+    $emoji.attr('alt', unicode);
   });
 
   return $.html();
