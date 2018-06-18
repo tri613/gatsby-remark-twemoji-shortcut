a5c473a71396fa359156aa0e7bbbdd180398023d
Trina Lu <tri613@gmail.com>
2018-06-18 19:32:36 +0800

    modify comments

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const { CodeBlockExtractor, shortcutToTwemoji } = require('./lib');
 function mutateSource({ markdownNode }, pluginOptions) {
   const content = markdownNode.internal.content;
 
-  // for skipping code block and keeping underscore shortcuts (e.g. :1st_place_medal:)
+  // for skipping code blocks and keeping underscore shortcuts (e.g. :1st_place_medal:)
   const extractor = new CodeBlockExtractor();
   const extracted = extractor.extract(content);
   const twittered = shortcutToTwemoji(extracted, pluginOptions);
